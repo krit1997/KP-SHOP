2880004b35570550c6d656cb4573688d3e392a12
krit <krit.t@extend-it-resource.com>
2025-10-19 21:01:26 +0700

    first commit

---
+++ src/main/java/com/example/kp_shop/KpShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.kp_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kp_shop/KpShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kp_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
